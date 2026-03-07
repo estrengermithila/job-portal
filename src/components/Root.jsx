6e7d9f7b892d6323965ca65825960eea5ddf3d64
estrengermithila <mithilafarjanam20@gmail.com>
2026-03-08 01:31:06 +0600

    commited

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div>
+        <div >
             <Navbar />
             <Outlet />
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,28 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement:<h1 >Router not found</h1>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/register',
+        element:<Register/>
+      },
+      {
+        path:'/login',
+        element:<Login/>
+      },
+    ]
   },
 ]);
 createRoot(document.getElementById('root')).render(
